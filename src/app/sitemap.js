d13bb60f5638e69905313a5a5dfef8a4386d32db
infancysulfur <yunhyun97@gmail.com>
2026-02-19 23:05:55 +0900

    Add : SEO

---
+++ src/app/sitemap.js
@@ -0,0 +1,7 @@
+export default function sitemap() {
+  return [
+    { url: 'https://magtool.vercel.app/', lastModified: new Date() },
+    { url: 'https://magtool.vercel.app/extractor', lastModified: new Date() },
+    { url: 'https://magtool.vercel.app/mixer', lastModified: new Date() },
+  ];
+}
